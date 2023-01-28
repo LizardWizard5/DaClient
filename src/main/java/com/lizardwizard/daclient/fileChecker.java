f7860501d899dec4b104c40bcac35ce14e372997
LizardWizard <ericsamonkey@gmail.com>
2023-01-27 19:40:46 -0500

    Test

--- src/main/java/com/lizardwizard/daclient/fileChecker.java
+++ src/main/java/com/lizardwizard/daclient/fileChecker.java
@@ -1,7 +1,14 @@
 package com.lizardwizard.daclient;
 
+import java.io.File; // Imports the fileio library
 public class fileChecker {
     public static void versionComparer(){
-
+    File versionFile = new File("version.txt");
+        if(versionFile.exists()){
+            System.out.println("File found!");
+        }
+        else{
+            //mkdir
+        }
     }
 }
